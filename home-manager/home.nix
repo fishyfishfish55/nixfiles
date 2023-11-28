7525ccc0882e10246c47a23fbf1443d6407c4cec
fishyfishfish55 <l.lacosct@gmail.com>
2023-11-28 13:31:04 -0500

    initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,230 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "genius";
+    homeDirectory = "/home/genius";
+  };
+
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "Tokyonight-Dark-BL";
+      package = pkgs.tokyo-night-gtk;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+     "org/gnome/desktop/background" = {
+     	picture-uri = "file:///home/genius/.local/share/backgrounds/2023-09-02-17-25-58-citypaper.jpg";
+     	picture-uri-dark = "file:///home/genius/.local/share/backgrounds/2023-09-02-17-25-58-citypaper.jpg";
+     };
+     "org/gnome/shell" = {
+       disable-user-extensions = false;
+     };
+   };
+  
+  home.packages = with pkgs; [
+    bat
+    cava
+    tldr
+    firefox
+    reaper
+    iosevka
+    nerdfonts
+    obsidian
+    libreoffice-fresh
+    discord
+    mpv
+    youtube-dl
+    doomretro
+    # Helix packages
+    # TODO: move to ExtraPackages
+    marksman
+    rust-analyzer-unwrapped
+    lua-language-server
+    nil
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "fishyfishfish55";
+    userEmail = "l.lacossct@gmail.com";
+  };
+
+  programs.gh = {
+    enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+    enableAutosuggestions = true;
+    shellAliases = {
+      md = "mkdir -p";
+      cat = "bat";
+      du = "dust";
+      df = "duf";
+      rf = "rm -rf";
+      rd = "rmdir";
+      icat = "kitty +kitten icat";
+      update = "sudo nixos-rebuild switch";
+      musique = "mpv --config=no --quiet --vo=tct --really-quiet --lavfi-complex='[aid1]asplit[ao][a1];[a1]avectorscope=r=25:m=lissajous_xy:bc=100:gc=100:rc=75:bf=5:gf=3:rf=1:zoom=1[vo]'";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "plugins/sudo"; tags = [ "from:oh-my-zsh" ]; }
+      ];
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+    prefix = "C-Space";
+    keyMode = "vi";
+  };
+
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      add_newline = true;
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    theme = "Tokyo Night";
+    shellIntegration.enableZshIntegration = true;
+    font = {
+    	name = "Iosevka";
+    	size = 11;
+    };
+    settings = {
+      hide_window_decorations = true;
+      remember_window_size = false;
+      initial_window_width = "80c";
+      initial_window_height = "24c";
+      background_opacity = "0.95";
+    };
+  };
+
+  programs.helix = {
+    enable = true;
+    package = inputs.helix.packages.${pkgs.system}.default;
+    settings = {
+      theme = "tokyonight";
+      editor = {
+        line-number = "relative";
+      	cursor-shape = {
+      	  insert = "bar";
+      	  normal = "block";
+      	  select = "underline";
+      	};
+      	file-picker.hidden = false;
+      };
+    };
+  };
+
+#   programs.eww = {
+#    enable = true;
+#    package = inputs.eww.packages.${pkgs.system}.default;
+#  };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    package = inputs.swayfx.packages.${pkgs.system}.default;
+    config = rec {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "kitty"; 
+      startup = [
+        # Launch Firefox on start
+        {command = "firefox";}
+      ];
+    };
+  };
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,226 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-pc-laptop
+    inputs.hardware.nixosModules.common-pc-laptop-ssd
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  nix.settings = {
+    # Enable flakes and new 'nix' command
+    experimental-features = "nix-command flakes";
+    # Deduplicate and optimize nix store
+    auto-optimise-store = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    curl
+    git
+    distrobox
+    gnome.gnome-tweaks
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.appindicator
+    gnomeExtensions.gesture-improvements
+    gnomeExtensions.rounded-window-corners
+    fd
+    ripgrep
+    jq
+    fzf
+    du-dust
+    duf
+    btop
+    neofetch
+    python3
+    inputs.home-manager.packages.${pkgs.system}.default
+    # inputs.swayfx.packages.${pkgs.system}.default
+    (retroarch.override {
+      cores = with libretro; [
+        bsnes
+      	nestopia
+      	genesis-plus-gx
+      ];
+    })
+    jdk17
+    wireshark
+    zip
+    unzip
+    spotify
+    config.nur.repos.milahu.spotify-adblock
+  ];
+
+  # Steam
+  programs.steam = {
+    enable = true;
+    dedicatedServer.openFirewall = true;
+    remotePlay.openFirewall = true;
+  };
+  hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam uses
+
+  # Z Shell
+  programs.zsh.enable = true;
+  environment.shells = with pkgs; [ zsh ];
+
+  # Hostname
+  networking.hostName = "midnight";
+  networking.extraHosts = ''
+    23.215.223.144 media.steampowered.com
+  '';
+  networking.networkmanager.enable = true;
+
+  services.nscd = {
+    enableNsncd = true;
+  };
+
+  # Date and time
+  time.timeZone = "America/New_York";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Desktop environment
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Bootloader stuff
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-4e035de1-8190-4e3f-8eb8-4ff52729addb".device = "/dev/disk/by-uuid/4e035de1-8190-4e3f-8eb8-4ff52729addb";
+  boot.initrd.luks.devices."luks-4e035de1-8190-4e3f-8eb8-4ff52729addb".keyFile = "/crypto_keyfile.bin";
+
+  # Plymouth
+  boot.plymouth = {
+    enable = true;
+  };
+
+  # Services
+  # Polkit
+  security.polkit.enable = true;  
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Audio
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+
+  services.udev.extraRules = ''
+    KERNEL=="rtc0", GROUP="audio"
+    KERNEL=="hpet", GROUP="audio"
+  '';
+
+  security.pam.loginLimits = [
+    { domain = "@audio"; item = "memlock"; type = "-"   ; value = "unlimited"; }
+    { domain = "@audio"; item = "rtprio" ; type = "-"   ; value = "99"       ; }
+    { domain = "@audio"; item = "nofile" ; type = "soft"; value = "99999"    ; }
+    { domain = "@audio"; item = "nofile" ; type = "hard"; value = "524288"    ; }
+  ];
+
+  # Flatpak
+  services.flatpak.enable = true;  
+
+  # Configure your system-wide user settings (groups, etc)
+  users.users = {
+    genius = {
+      isNormalUser = true;
+      shell = pkgs.zsh;
+      description = "genius";
+      extraGroups = [ "networkmanager" "wheel" "wireshark" "audio" ];
+    };
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      genius = import ../home-manager/home.nix;
+    };
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+}
