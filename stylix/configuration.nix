88eb548371d5f95d3cc6a7335968bb6794a441d3
fishyfishfish55 <l.lacosct@gmail.com>
2023-12-19 13:18:44 -0500

    Limit stylix styling

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -6,6 +6,7 @@
     image = /home/genius/Pictures/wallpaper/nord-iceberg.jpg;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    autoEnable = true;
     cursor = {
       package = pkgs.nordzy-cursor-theme;
       name = "Nordzy-cursors";
@@ -34,6 +35,17 @@
         popups = 11;
         terminal = 11;
       };
+    };
+    targets = {
+      plymouth = {
+        enable = true;
+        blackBackground = false;
+      };
+      gnome.enable = true;
+      gtk.enable = true;
+      sway.enable = true;
+      bat.enable = true;
+      fzf.enable = true;
     };
   };
 }
