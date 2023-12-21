5a382f6bf74796e7de411d273a2924e21f010ed4
fishyfishfish55 <l.lacosct@gmail.com>
2023-12-21 14:23:59 -0500

    enable autoupgrade

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -236,6 +236,18 @@
     };
   };
 
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
