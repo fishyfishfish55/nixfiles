3e425f1e387d4c5821ddc32f042d05c9e4cb5920
fishyfishfish55 <l.lacosct@gmail.com>
2023-12-21 14:11:45 -0500

    remove eww

--- flake.nix
+++ flake.nix
@@ -21,8 +21,8 @@
     swayfx.url = "github:WillPower3309/swayfx";
     helix.url = "github:helix-editor/helix";
     steam-tui.url = "github:dmadisetti/steam-tui";
-    nixpkgs-wayland.url = "github:nix-community/nixpkgs-sway";
-    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
+    # nixpkgs-wayland.url = "github:nix-community/nixpkgs-sway";
+    # nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
 
   };
 
@@ -34,7 +34,7 @@
     nur,
     swayfx,
     helix,
-    nixpkgs-wayland,
+    # nixpkgs-wayland,
     stylix,
     steam-tui,
     ...
