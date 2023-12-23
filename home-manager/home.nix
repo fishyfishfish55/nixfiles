51daaeaf0c007326e77dda882a11f50e7f8716c1
fishyfishfish55 <l.lacosct@gmail.com>
2023-12-23 18:12:38 -0500

    add warnings for old nix commands

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -99,6 +99,12 @@
       icat = "kitty +kitten icat";
       update = "sudo nixos-rebuild switch --update-input nixpkgs --commit-lock-file --show-trace";
       musique = "mpv --config=no --quiet --vo=tct --really-quiet --lavfi-complex='[aid1]asplit[ao][a1];[a1]avectorscope=r=25:m=lissajous_xy:bc=100:gc=100:rc=75:bf=5:gf=3:rf=1:zoom=1[vo]'";
+      # Old nix commands - reevaluate.
+      nix-channel = "echo 'whoa there, buddy.  Use flake inputs instead'";
+      nix-env = "echo 'whoa there, buddy. Did you mean nix profile?'";
+      nix-shell = "echo 'whoa there, buddy. Do some research first'";
+      nix-build = "echo 'whoa there, buddy. Do some research first - check nix develop, nix shell, and nix run'";
+      nix-collect-garbage = "echo 'whoa there, buddy. Did you mean nix store gc --debug?'";
     };
     oh-my-zsh = {
       enable = true;
