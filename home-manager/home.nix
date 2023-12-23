f4eba79dde8da31d745a0e82f3a87c9b2231267a
fishyfishfish55 <l.lacosct@gmail.com>
2023-12-23 14:10:29 -0500

    configure waybar

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -205,7 +205,12 @@
         }
       ];
       window.titlebar = false;
-      bars = [];
+      bars = [     
+        {
+          id = "waybar";
+          command = "waybar";
+        }      
+      ];
     };
   };
 
@@ -257,15 +262,40 @@
       enable = true;
       target = "sway-session.target";
     };
-    settings = {
-      waybar = {
-        # id = "waybar";
-        # ipc = true;
-        layer = "top";
-        position = "top";
-        modules-left = [ "sway/workspaces" ];
-        modules-center = [ "sway/window" ];
-        modules-right = [ "clock" "backlight" "wireplumber" "battery" "tray" ];
+    settings.waybar = {
+      id = "waybar";
+      ipc = true;
+      layer = "top";
+      position = "top";
+      modules-left = [ "sway/workspaces" ];
+      modules-center = [ "sway/window" ];
+      modules-right = [ "clock" "wireplumber" "backlight" "battery" "tray" ];
+      "sway/workspaces" = {
+        format-icons = {
+          default = "○";
+          focused = "◎";
+        };
+        format = "{icon}";
+      };
+      clock = {
+        interval = 1;
+        format = "{:%I:%M:%S}";
+        tooltip-format = "{:%d.%m.%y}";
+      };
+      backlight = {
+        format = "{icon}";
+        format-icons = [ "󰃚" "󰃛" "󰃜" "󰃝" "󰃞" "󰃟" "󰃠" ];
+      };
+      wireplumber = {
+        format = "{icon} {volume}%";
+        format-muted = "󰝟  {volume}%";
+        format-icons = [ "󰕿" "󰖀" "󰕾" ];
+      };
+      battery = {
+        format = "{icon}  {capacity}%";
+        format-charging = "{icon}󱐋 {capacity}%";
+        interval = 2;
+        format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" ];
       };
     };
   };
