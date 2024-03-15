d053fe9b7b39086a8b4a2e523502916d5d9b6803
fishyfishfish55 <l.lacosct@gmail.com>
2024-03-15 15:06:51 -0400

    limit generations

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -177,7 +177,10 @@
   };
 
   # Bootloader stuff
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 5;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
